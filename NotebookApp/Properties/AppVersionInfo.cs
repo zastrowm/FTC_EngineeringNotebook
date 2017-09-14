024fe1199262952ce42eff306d29dec52e1b25ff
Mackenzie Zastrow <zastrowm+sourcecontrol@gmail.com>
2017-09-13 21:09:33 -0500

    Update version number

--- NotebookApp/Properties/AppVersionInfo.cs
+++ NotebookApp/Properties/AppVersionInfo.cs
@@ -8,7 +8,7 @@ namespace NotebookApp.Properties
   {
     public const string Version = "1.0";
     public const string Label = "r";
-    public const string Build = "12";
+    public const string Build = "13";
 
     public const string EmailBase64 = "c3VwcG9zYWJsZXRodW1ic2Z0Y0BnbWFpbC5jb20=";
   }
